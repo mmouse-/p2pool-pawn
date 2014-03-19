f13bd9ae7a162e68ee361203507413f49ff2ce14
paniczklos <panicz.klos@gmail.com>
2014-03-19 09:50:35 +0100

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -869,7 +869,7 @@ nets = dict(
         CHAIN_LENGTH=24*60*60//20, # shares
         REAL_CHAIN_LENGTH=24*60*60//20, # shares #CHAIN_LENGTH & REAL_CHAIN_LENGTH are set up to allow for 3 Hour PPLNS.
         TARGET_LOOKBEHIND=20, # is set to 30 (shares) giving a 300 second (5min) difficulty adjustment.
-        SPREAD=10, # blocks #SPREAD=30 block every 60 seconds 600/60=10 10x3=30 because bitcoin's SPREAD=3 block every 600 seconds and litecoin'sSPREAD=12 block every 150 seconds 600/150=4 4x3=12
+        SPREAD=12, # blocks #SPREAD=30 block every 60 seconds 600/60=10 10x3=30 because bitcoin's SPREAD=3 block every 600 seconds and litecoin'sSPREAD=12 block every 150 seconds 600/150=4 4x3=12
         IDENTIFIER='7061776e7061776e'.decode('hex'), #some random s-it (I think its used to identify others p2pool's mining this coin)
         PREFIX='6e7061776e706177'.decode('hex'), #IDENTIFIER & PREFIX: P2Pool will only sync with other nodes who have Identifier and Prefix matching yours (and using same p2p port).. if any of the above values change, a new identifier & prefix need to be created in order to prevent problems.
         P2P_PORT=7060, #port that p2pool is comunicating on with other p2pools
